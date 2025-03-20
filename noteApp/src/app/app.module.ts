7ddee6ab5a5854e1a606875ee69981b46d859093
Andile <andilelethumusa@gmail.com>
2025-03-20 11:20:48 +0200

    Components

--- noteApp/src/app/app.module.ts
+++ noteApp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './components/form/form.component';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
