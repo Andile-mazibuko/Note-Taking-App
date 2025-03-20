1a7af1814270c08bf608a117be0da12aefed084c
Andile <andilelethumusa@gmail.com>
2025-03-20 11:56:57 +0200

    create note methods implemented

--- noteApp/src/app/components/form/form.component.ts
+++ noteApp/src/app/components/form/form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Note } from '../../interfaces/note';
+import { NoteService } from '../../services/note.service';
 
 @Component({
   selector: 'app-form',
@@ -7,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './form.component.scss'
 })
 export class FormComponent implements OnInit{
+  
+  noteForm!: FormGroup;
+  noteCreated!: Note;
+  constructor(private noteService: NoteService,private fb: FormBuilder){}
 
   ngOnInit(): void {
-  
+    this.noteForm = this.fb.group({
+      title: ['',Validators.required],
+      content: ['',Validators.required]
+    })
   }
 
-  
+  createNewNote()
+  {
+    this.noteService.createNote(this.noteForm.value); 
+  }
+
+
+
 }
--- noteApp/src/app/services/note.service.ts
+++ noteApp/src/app/services/note.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Note } from '../interfaces/note';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteService {
+  notes: Note[] = [];
+  behaviorSubject = new BehaviorSubject<Note[]>([]);    //ToTest: parse in this.notes instead of [] 
 
   constructor() { }
+
+  createNote(note: Note){
+    note.id = new Date().getTime(); // create a unique id based on time the note was created
+    this.notes.push(note);
+    this.behaviorSubject.next(this.notes);
+  }
+
+
 }
