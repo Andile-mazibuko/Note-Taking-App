9862fa38c964fcd8ab707f2d7bc0829ff6dcca5c
Andile <andilelethumusa@gmail.com>
2025-03-23 20:13:32 +0200

    Error handling

--- noteApp/src/app/app-routing.module.ts
+++ noteApp/src/app/app-routing.module.ts
@@ -3,8 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { FormComponent } from './components/form/form.component';
 import { ListComponent } from './components/list/list.component';
 import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-const routes: Routes = [{path:'', component: WelcomePageComponent},{path: 'form', component: FormComponent},{path: 'list', component: ListComponent}];
+const routes: Routes = [{path:'', component: WelcomePageComponent},{path: 'form', component: FormComponent},{path: 'list', component: ListComponent},{path:'**', title: 'Error 404 - page Not Found', component: PageNotFoundComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
